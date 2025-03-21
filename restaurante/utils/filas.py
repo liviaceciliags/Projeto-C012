2f31e51cc897e11cbe9e159dcb98dd201fd40f30
Matheus Renó Torres <124406908+RennowT@users.noreply.github.com>
2025-03-20 23:36:27 -0300

    Updates the filas file by adding the FilaSolicitacoes class

--- restaurante/utils/filas.py
+++ restaurante/utils/filas.py
@@ -19,5 +19,15 @@ class FilaPedidosProntos:
     def adicionar_pedido_pronto(self, pedido):
         self._fila.put(pedido)
 
-    def obter_proximo_pedido_pronto(self):
-        return self._fila.get()
+    def obter_proximo_pedido_pronto(self, block=True, timeout=None):
+        return self._fila.get(block, timeout)
+    
+class FilaSolicitacoes:
+    def __init__(self):
+        self._fila = queue.Queue()
+    
+    def adicionar_solicitacao(self, item):
+        self._fila.put(item)
+    
+    def get(self, block=True, timeout=None):
+        return self._fila.get(block, timeout)
